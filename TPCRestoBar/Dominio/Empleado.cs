9094eb5ac7b90798d6636995e7d8814dc4865199
Julian <julian.ocampo1219@gmail.com>
2026-05-25 19:18:15 -0300

    Agrego folder Dominio y Clase Empledo

---
+++ TPCRestoBar/Dominio/Empleado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TPCRestoBar.Dominio
+{
+    public class Empleado
+    {
+        public int id { get; set; }
+        public string nombre{ get; set; }
+        public string apellido { get; set; }
+        public string usuario { get; set; }
+        public string passaword { get; set; }
+        public string irold { get; set; }
+        public bool activo { get; set; }
+        
+    }
+}
