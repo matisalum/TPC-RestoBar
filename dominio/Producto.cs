b6cc1700037603d750252daaa56e0e7f66b09a06
Matias Salum <matiussalum@gmail.com>
2026-05-25 19:34:19 -0300

    clase Producto

---
+++ dominio/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    internal class Producto
+    {
+        public int id {  get; set; }
+        public string nombre { get; set; }
+        public decimal precio {  get; set; }
+        public int stock { get; set; }
+        bool activo { get; set; }
+    }
+}
