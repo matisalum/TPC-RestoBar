17660d8b9e358362a099e1bcabf68cec60ae7e1c
Julian <julian.ocampo1219@gmail.com>
2026-05-25 19:39:33 -0300

    clase Mesa

---
+++ dominio/Mesa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    internal class Mesa
+    {
+        public int id { get; set; }
+        public int numero { get; set; }
+        public int capadicad { get; set; }
+        public bool estado { get; set; }
+
+    }
+
+}
