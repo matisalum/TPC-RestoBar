a026164fd37f4d6e024bef2f04053761ee7998f4
Javier Altamirano <javi.alt87@gmail.com>
2026-05-25 19:54:18 -0300

    Nueva clase pedido

---
+++ dominio/Pedido.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    internal class Pedido
+    {
+        public int idPedido { get; set; }
+
+        public Mesa mesaPedido { get; set; }
+
+        public Empleado empleadoMesa { get; set; }
+
+        DateTime fechaPedido { get; set; }
+
+       public bool estadoPedido { get; set; }
+
+    }
+}
